cec1f244dbb68acc4bbf69b33c134ff9fe3cbfa2
Diogo Martins <me.martinsfe@gmail.com>
2022-08-29 13:53:04 -0300

    chore: finish task 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species
+    .find((specie) => specie.name === animal)
+    .residents
+    .every((resident) => resident.age >= age);
 }
 
+// console.log(getAnimalsOlderThan('lions', 10));
 module.exports = getAnimalsOlderThan;
