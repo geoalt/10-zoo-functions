e08c1b2723df88c84d2a8052c2a198da6ff457fd
Diogo Martins <me.martinsfe@gmail.com>
2022-08-29 14:28:43 -0300

    chore: finish task 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const searchResult = employees.find((employee) =>
+    employee.firstName === employeeName
+    || employee.lastName === employeeName);
+
+  return searchResult || {};
 }
 
+// console.log(getEmployeeByName('Nelson'));
 module.exports = getEmployeeByName;
