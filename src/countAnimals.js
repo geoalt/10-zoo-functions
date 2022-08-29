6ad00088dac1166f6681fa6c9e272ef5d040615c
Diogo Martins <me.martinsfe@gmail.com>
2022-08-29 20:18:52 -0300

    chore: finish task 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+const { species } = data;
+
+function countAnimals(animal = species) {
   // seu código aqui
+  if (!animal.specie) {
+    return animal.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const searchSpecie = species
+    .find((specie) => specie.name === animal.specie)
+    .residents;
+
+  return animal.sex
+    ? searchSpecie.filter((specie) => specie.sex === animal.sex).length
+    : searchSpecie.length;
 }
 
+console.log(countAnimals({ specie: 'penguins' }));
 module.exports = countAnimals;
