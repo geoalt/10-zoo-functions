b9af29d3e5b75cb9b7293d8c2892f364b7762b48
Diogo Martins <me.martinsfe@gmail.com>
2022-08-29 13:28:44 -0300

    chore: finish task 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const { species } = data;
 
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  return ids.map((id) => species
+    .filter((specie) => specie.id === id))
+    .flat();
+}
+console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'))
 module.exports = getSpeciesByIds;
