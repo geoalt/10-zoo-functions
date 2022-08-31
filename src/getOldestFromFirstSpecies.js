3f3790e414049b186af35710b47910389e91f274
Diogo Martins <me.martinsfe@gmail.com>
2022-08-30 23:04:53 -0300

    chore: finish task 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+
+const getFirstAnimal = (employeeId) => {
+  const search = employees.find((employee) => employee.id === employeeId);
+  return search && search.responsibleFor[0];
+};
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const specieId = getFirstAnimal(id);
+  const oldestAnimal = species.find((specie) => specie.id === specieId)
+    .residents
+    .reduce((acc, curr) =>
+      (acc.age > curr.age ? acc : curr));
+
+  return Object.values(oldestAnimal);
 }
 
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
