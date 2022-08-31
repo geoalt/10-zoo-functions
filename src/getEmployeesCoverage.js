f48ea8c9093624a83335f795a91a4f3422d43f19
Diogo Martins <me.martinsfe@gmail.com>
2022-08-31 01:07:52 -0300

    chore: finish task 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,53 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species } = data;
+const { employees } = data;
+
+const getEmployee = (obj) => {
+  const { name, id } = obj;
+
+  const searchByName = employees.find((employee) => employee.firstName === name
+    || employee.lastName === name);
+  const searchById = employees.find((employee) => employee.id === id);
+
+  console.log(searchById, searchByName);
+
+  if (!searchById && !searchByName) {
+    throw new Error('Informações inválidas');
+  }
+
+  return searchByName || searchById;
+};
+
+const getSpecies = (animalsList) => animalsList.map((animal) => species
+  .filter((specie) => specie.id === animal))
+  .flat();
+
+const printAllEmployees = () => employees.map((employee) => ({
+  id: employee.id,
+  fullName: `${employee.firstName} ${employee.lastName}`,
+  species: getSpecies(employee.responsibleFor)
+    .map((specie) => specie.name),
+  locations: getSpecies(employee.responsibleFor)
+    .map((specie) => specie.location),
+}));
+
+function getEmployeesCoverage(obj) {
+  const print = {};
+
+  if (!obj) {
+    return printAllEmployees();
+  }
+
+  const employeeInfo = getEmployee(obj);
+  const speciesInfo = getSpecies(employeeInfo.responsibleFor);
+
+  print.id = employeeInfo.id;
+  print.fullName = `${employeeInfo.firstName} ${employeeInfo.lastName}`;
+  print.species = speciesInfo.map((specie) => specie.name);
+  print.locations = speciesInfo.map((specie) => specie.location);
+
+  return print;
 }
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -18,5 +18,5 @@ function getOldestFromFirstSpecies(id) {
       .reduce((acc, curr) => (acc.age > curr.age ? acc : curr)));
 }
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
